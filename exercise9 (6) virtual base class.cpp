b469ab58c1f89f0c1fe8ae5412e41ab266cc3dbc
Nallamilli S V S R Krushik Reddy <52321852+KrushikReddyNallamilli@users.noreply.github.com>
2020-04-27 12:19:17 +0530

    Add files via upload

---
+++ exercise9 (6) virtual base class.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+class A{
+	protected:
+		int a;
+};
+class B:public virtual A{
+	protected:
+		int b;
+};
+class C:public virtual A {
+	protected:
+		int c;
+};
+class D:public B,C{
+	public:
+	int d;
+	void get_data(){
+		cout<<"enter the value of a,b,c and d";
+		cin>>a>>b>>c>>d;
+	}
+	void display(){
+		cout<<"The values of a,b,c and d are :"<<a<<"\t"<<b<<"\t"<<c<<"\t"<<d;		
+	}
+};
+int main(){
+	D b1;
+	b1.get_data();
+	b1.display();
+}
